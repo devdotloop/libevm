5a57f6df16a945980da6b9cf969ce5c8c9cb6a08
Arran Schlosberg <me@arranschlosberg.com>
2024-11-19 20:16:00 +0000

    test: helpers plus basic test of `WithNextNearestAncestor`

---
+++ core/state/snapshot/snapshot.libevm_test.go
@@ -0,0 +1,202 @@
+// Copyright 2024 the libevm authors.
+//
+// The libevm additions to go-ethereum are free software: you can redistribute
+// them and/or modify them under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// The libevm additions are distributed in the hope that they will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+// General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see
+// <http://www.gnu.org/licenses/>.
+
+package snapshot
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/ava-labs/libevm/common"
+	"github.com/ava-labs/libevm/core/types"
+	"github.com/ava-labs/libevm/ethdb/memorydb"
+)
+
+type hashLike interface {
+	common.Hash | byte | int | string
+}
+
+func asHash[T hashLike](root T) common.Hash {
+	switch r := any(root).(type) {
+	case common.Hash:
+		return r
+	case byte:
+		return common.Hash{r}
+	case int:
+		// It doesn't matter if this truncates as we only expect to use it with
+		// single-byte hex literals.
+		return common.Hash{byte(r)}
+	case string:
+		var h common.Hash
+		copy(h[:], []byte(r))
+		return h
+	default:
+		return common.Hash{}
+	}
+}
+
+// update calls [Tree.Update] to create a layer with a single account, derived
+// from `label`, which MUST be distinct and SHOULD be human-readable.
+func update[T hashLike](tb testing.TB, tree *Tree, root byte, parent T, label string, opts ...LibEVMOption) {
+	tb.Helper()
+
+	accounts := map[common.Hash][]byte{
+		asHash(label): []byte(label),
+	}
+	require.NoErrorf(
+		tb,
+		tree.Update(asHash(root), asHash(parent), nil, accounts, nil, opts...),
+		"%T.Update(..., [%q], ...)", tree, label,
+	)
+}
+
+type labelSet map[string]struct{}
+
+// labelsOf assumes that the snapshot was created with [update] and returns all
+// of the labels in the layer. If no flattening has been performed then only a
+// single label is expected.
+func labelsOf(tb testing.TB, snap Snapshot) labelSet {
+	tb.Helper()
+	require.NotNil(tb, snap, "Snapshot for label extraction")
+
+	it := snap.(snapshot).AccountIterator(common.Hash{}) //nolint:forcetypeassert // Known invariant
+	defer it.Release()
+
+	labels := make(labelSet)
+	for it.Next() {
+		if err := it.Error(); err != nil {
+			tb.Fatalf("%T.Error() = %v", it, err)
+		}
+		labels[string(it.Account())] = struct{}{}
+	}
+	return labels
+}
+
+// labelsOfPath is the multi-layer equivalent of [labelsOf].
+func labelsOfPath(tb testing.TB, layers []Snapshot) []labelSet {
+	tb.Helper()
+
+	labels := make([]labelSet, len(layers))
+	for i, l := range layers {
+		labels[i] = labelsOf(tb, l)
+	}
+	return labels
+}
+
+// asLabelSets converts the slices of labels into sets; this is useful for
+// defining test expectations.
+func asLabelSets(labels ...[]string) []labelSet {
+	sets := make([]labelSet, len(labels))
+	for i, lbls := range labels {
+		sets[i] = make(labelSet)
+		for _, l := range lbls {
+			sets[i][l] = struct{}{}
+		}
+	}
+	return sets
+}
+
+func newEmptyTree(tb testing.TB) *Tree {
+	tb.Helper()
+	disk := memorydb.New()
+	tree, err := New(
+		Config{
+			CacheSize: 16, // Mb (arbitrary but non-zero)
+		},
+		disk,
+		nil, // triedb unnecessary as we're not rebuilding
+		types.EmptyRootHash,
+	)
+	require.NoError(tb, err, "New()")
+	return tree
+}
+
+// Labels used by [ambiguousTree].
+const (
+	A       = "A"
+	B0      = "B0"
+	B1      = "B1"
+	CFromB0 = "C from B0"
+	CFromB1 = "C from B1"
+	DViaB0  = "D0 (via B0)"
+	DViaB1  = "D1 (via B1)"
+	EFromD0 = "E from D0"
+	EFromD1 = "E from D1"
+)
+
+// ambiguousTree returns a tree with ambiguous roots 0xC and 0xE. They are,
+// however, created via [update] with distinct labels so they can be
+// differentiated with deeper inspection.
+//
+//	A
+//	├── B0
+//	│   └── C*
+//	│       └── D0
+//	│           └── E**
+//	└── B1
+//	    └── C*
+//	        └── D0
+//	            └── E**
+func ambiguousTree(tb testing.TB) *Tree {
+	tb.Helper()
+	tree := newEmptyTree(tb)
+	update(tb, tree, 0xA, tree.DiskRoot(), A)
+	update[byte](tb, tree, 0xB0, 0xA, B0)
+	update[byte](tb, tree, 0xB1, 0xA, B1)
+	update[byte](tb, tree, 0xC, 0xB0, CFromB0)
+	update[byte](tb, tree, 0xC, 0xB1, CFromB1)
+
+	update[byte](tb, tree, 0xD0, 0xC, DViaB0, WithNextNearestAncestor(asHash(0xb0)))
+	update[byte](tb, tree, 0xD1, 0xC, DViaB1, WithNextNearestAncestor(asHash(0xb1)))
+
+	update[byte](tb, tree, 0xE, 0xD0, EFromD0)
+	update[byte](tb, tree, 0xE, 0xD1, EFromD1)
+	return tree
+}
+
+func TestUpdateWithNextNearestAncestor(t *testing.T) {
+	tree := ambiguousTree(t)
+	tests := []struct {
+		root byte
+		want [][]string
+	}{
+		{
+			root: 0xD0,
+			want: [][]string{
+				{DViaB0},
+				{CFromB0},
+				{B0},
+				{A},
+			},
+		},
+		{
+			root: 0xD1,
+			want: [][]string{
+				{DViaB1},
+				{CFromB1},
+				{B1},
+				{A},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		layers := tree.Snapshots(asHash(tt.root), 999, true)
+		assert.Equalf(t, asLabelSets(tt.want...), labelsOfPath(t, layers), "Snapshots from %#x down", tt.root)
+	}
+}
